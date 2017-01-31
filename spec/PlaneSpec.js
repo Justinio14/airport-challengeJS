a93ce8d33bb53efd319f1b99d7edd3c80fd57fd7
Joe Marriott <marriott90@gmail.com>
2017-01-31 16:46:27 +0000

    user story 3 complete

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,20 +1,20 @@
 'use strict';
 
-describe('Plane',function(){
-  var plane;
-  var airport;
-  beforeEach(function(){
-    plane = new Plane();
-    airport = jasmine.createSpyObj('airport',['clearForLanding', 'clearForTakeOff']);
-  });
-  it('can land at airport', function(){
-    plane.land(airport);
-    expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
-  });
-  it('can takeoff from an airport', function(){
-    plane.land(airport);
-    plane.takeoff();
-    expect(airport.clearForTakeOff).toHaveBeenCalled();
-  });
+describe('Plane', function() {
+    var plane;
+    var airport;
+    beforeEach(function() {
+        plane = new Plane();
+        airport = jasmine.createSpyObj('airport', ['clearForLanding', 'clearForTakeOff']);
+    });
+    it('can land at airport', function() {
+        plane.land(airport);
+        expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+    });
+    it('can takeoff from an airport', function() {
+        plane.land(airport);
+        plane.takeoff();
+        expect(airport.clearForTakeOff).toHaveBeenCalled();
+    });
 
 });
--- src/Plane.js
+++ src/Plane.js
@@ -1,10 +1,10 @@
 'use strict';
 
-function Plane(){}
-Plane.prototype.land = function(airport){
-  airport.clearForLanding(this);
+function Plane() {}
+Plane.prototype.land = function(airport) {
+    airport.clearForLanding(this);
     this._location = airport;
 };
-Plane.prototype.takeoff = function(airport){
+Plane.prototype.takeoff = function(airport) {
     this._location.clearForTakeOff('');
 };
