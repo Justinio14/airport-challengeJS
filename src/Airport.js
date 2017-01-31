e3e90e8a9804df7e5f703ec1d5bb82e123722e1e
Justinio14 <jbutwell@hotmail.com>
2017-01-31 16:11:11 +0000

    User storty 2 complete

--- src/Airport.js
+++ src/Airport.js
@@ -6,5 +6,7 @@ function Airport(){
 Airport.prototype.planes = function(){ return this._hangar; };
 Airport.prototype.clearForLanding = function(plane){
   this._hangar.push(plane);
-
+};
+Airport.prototype.clearForTakeOff = function(plane) {
+  this._hangar = [];
 };
